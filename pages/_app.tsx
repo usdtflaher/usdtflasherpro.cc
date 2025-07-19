016e1ce31deecd9754401898de05812a9d823d1c
NfonJoseph17 <81618540+NfonJoseph17@users.noreply.github.com>
2025-07-19 14:12:12 +0100

    Add Google Analytics tracking code (gtag.js) to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,19 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/usdt.ico" />
+        
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16955353742"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16955353742');
+            `,
+          }}
+        />
       </Head>
       <div className={inter.className}>
         <Component {...pageProps} />
